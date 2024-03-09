7d998b4d149cb468b8c6abac69bea8f4523cd988
jasozh <48730262+jasozh@users.noreply.github.com>
2024-03-09 11:18:35 -0500

    Set up skeleton code for project and models (#5)
    Co-authored-by: laura gong <laurandraaa@users.noreply.github.com>

---
+++ ml/convert-svg/model.py
@@ -0,0 +1,11 @@
+def convert_svg(f: bytes) -> bytes:
+    """
+    Converts a monochrome JPG/PNG file into an SVG
+
+    Args:
+        f: binary file representing an image JPG or PNG
+
+    Returns:
+        A binary file representing an SVG
+    """
+    pass
