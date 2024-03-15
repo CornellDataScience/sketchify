080e0b77a87a83c706c8474903c305e91fb7c1e8
cjt76 <cjt76@cornell.edu>
2024-03-15 17:30:27 -0400

    Implement ImageIO class
    -handle input and output
    
    -convert image to grayscale

--- ml/edge-detection/model.py
+++ ml/edge-detection/model.py
@@ -1,3 +1,5 @@
+from image_io import ImageIO
+
 def edge_detection(f: bytes) -> bytes:
     """
     Converts a full-color image into an image with only the image edges
@@ -8,4 +10,11 @@ def edge_detection(f: bytes) -> bytes:
     Returns:
         A binary file representing an image JPG or PNG
     """
-    pass
+    try:
+        color_im = ImageIO.read_image(f)
+    except FileNotFoundError:
+        print(f"Error: File '{f}' not found in the current directory.")
+        return
+
+    gray_im = ImageIO.convert_grayscale(color_im)
+    
