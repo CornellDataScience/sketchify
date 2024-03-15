066cc8756016da738aab8c94e4a39bba4a43ea4b
Darren Key <dyk34@cornell.edu>
2024-03-15 18:01:05 -0400

    Got edge_detection working with a sample test case

--- ml/edge-detection/model.py
+++ ml/edge-detection/model.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-# from image_io import ImageIO
-=======
->>>>>>> 9a4ee120ba02a05f92347a691fdeea7353d516ee
 import io
 from PIL import Image
 import cv2
@@ -23,9 +19,25 @@ def edge_detection(f: bytes) -> bytes:
 
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    edges = cv2.Canny(gray_image, 100, 200)
+
+    is_success, im_buf_arr = cv2.imencode(".jpg", edges)
+
+    byte_im = im_buf_arr.tobytes()
+
+    return byte_im
+
 
 if __name__ == "__main__":
-    with open("'../../public/Glazed-Donut.jpg'", 'rb') as f:
+    with open("../../public/Glazed-Donut.jpg", 'rb') as f:
         r = f.read()
 
-    edge_detection(r)
+    res = edge_detection(r)
+
+    image = np.array(Image.open(io.BytesIO(res)))
+
+    import matplotlib.pyplot as plt
+
+    plt.imshow(image, cmap="gray")
+
+    plt.show()
