9602ee5e3246fe9b6258b56ef9917eb019ce5e2f
3TTemi <temi.btw@gmail.com>
2024-03-21 01:23:25 -0400

    SVG conversion implemented with temporary files

--- ml/convert-svg/model.py
+++ ml/convert-svg/model.py
@@ -1,3 +1,9 @@
+import tempfile
+import os
+import vtracer
+from PIL import Image
+import numpy as np
+
 def convert_svg(f: bytes) -> bytes:
     """
     Converts a monochrome JPG/PNG file into an SVG
@@ -8,4 +14,38 @@ def convert_svg(f: bytes) -> bytes:
     Returns:
         A binary file representing an SVG
     """
-    pass
+
+    # Create a temporary file to write the input binary data (image)
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as tmp_input:
+            tmp_input.write(f)
+            tmp_input_path = tmp_input.name
+
+    # Define the output path for the SVG file
+    output_path = "../../ml/convert-svg/output.svg"
+
+     # Call the vtracer library to convert the temporary file to SVG
+    vtracer.convert_image_to_svg_py(tmp_input_path,
+                                    output_path,
+                                    colormode='binary'
+                                    )
+
+    # Read the generated SVG data as binary
+    with open(output_path, 'rb') as file:
+        binary_data = file.read()
+
+    # Delete temporary files
+    os.remove(tmp_input_path)
+    os.remove(output_path)
+
+    return binary_data
+
+
+if __name__ == "__main__":
+    input_path = "../../public/coloring002.jpg"
+    output_path = "../../public/coloring002.svg"
+
+    # Read as Binary into variable r 
+    with open(input_path, 'rb') as input_file:
+        input_binary = input_file.read()
+
+    res = convert_svg(input_binary)
