ea831b48a9a07e23317170b4742f595672b978a4
Jason Zheng <jasonz4200@gmail.com>
2024-03-22 16:08:15 -0400

    Delete

--- ml/edge-detection/model.py
+++ ml/edge-detection/model.py
@@ -15,7 +15,6 @@ def edge_detection(f: bytes) -> bytes:
     Returns:
         A binary file representing an image JPG or PNG
     """
-
     image = np.array(Image.open(io.BytesIO(f)))
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray_image, 100, 200)
@@ -23,14 +22,3 @@ def edge_detection(f: bytes) -> bytes:
     byte_im = im_buf_arr.tobytes()
 
     return byte_im
-
-
-# if __name__ == "__main__":
-#     with open("../../public/Glazed-Donut.jpg", 'rb') as f:
-#         r = f.read()
-
-#     res = edge_detection(r)
-#     image = np.array(Image.open(io.BytesIO(res)))
-
-#     plt.imshow(image, cmap="gray")
-#     plt.show()
