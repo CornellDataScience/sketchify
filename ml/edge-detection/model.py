ccb62e6b723b254a8b07cd639f9e5ac8e3ec5069
Laura Gong <lg443@cornell.edu>
2024-03-22 15:23:34 -0400

    formatting changes

--- ml/edge-detection/model.py
+++ ml/edge-detection/model.py
@@ -2,6 +2,7 @@ import io
 from PIL import Image
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def edge_detection(f: bytes) -> bytes:
@@ -16,28 +17,20 @@ def edge_detection(f: bytes) -> bytes:
     """
 
     image = np.array(Image.open(io.BytesIO(f)))
-
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
     edges = cv2.Canny(gray_image, 100, 200)
-
     is_success, im_buf_arr = cv2.imencode(".jpg", edges)
-
     byte_im = im_buf_arr.tobytes()
 
     return byte_im
 
 
-if __name__ == "__main__":
-    with open("../../public/Glazed-Donut.jpg", 'rb') as f:
-        r = f.read()
+# if __name__ == "__main__":
+#     with open("../../public/Glazed-Donut.jpg", 'rb') as f:
+#         r = f.read()
 
-    res = edge_detection(r)
+#     res = edge_detection(r)
+#     image = np.array(Image.open(io.BytesIO(res)))
 
-    image = np.array(Image.open(io.BytesIO(res)))
-
-    import matplotlib.pyplot as plt
-
-    plt.imshow(image, cmap="gray")
-
-    plt.show()
+#     plt.imshow(image, cmap="gray")
+#     plt.show()
