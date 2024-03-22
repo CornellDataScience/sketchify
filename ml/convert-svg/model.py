fb0e122d1f0aa7f3c540a66b01492d8e0776037b
Jason Zheng <jasonz4200@gmail.com>
2024-03-22 15:50:14 -0400

    Format

--- ml/convert-svg/model.py
+++ ml/convert-svg/model.py
@@ -4,6 +4,7 @@ import vtracer
 from PIL import Image
 import numpy as np
 
+
 def convert_svg(f: bytes) -> bytes:
     """
     Converts a monochrome JPG/PNG file into an SVG
@@ -17,13 +18,13 @@ def convert_svg(f: bytes) -> bytes:
 
     # Create a temporary file to write the input binary data (image)
     with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as tmp_input:
-            tmp_input.write(f)
-            tmp_input_path = tmp_input.name
+        tmp_input.write(f)
+        tmp_input_path = tmp_input.name
 
     # Define the output path for the SVG file
     output_path = "../../ml/convert-svg/output.svg"
 
-     # Call the vtracer library to convert the temporary file to SVG
+    # Call the vtracer library to convert the temporary file to SVG
     vtracer.convert_image_to_svg_py(tmp_input_path,
                                     output_path,
                                     colormode='binary'
@@ -44,7 +45,7 @@ if __name__ == "__main__":
     input_path = "../../public/coloring002.jpg"
     output_path = "../../public/coloring002.svg"
 
-    # Read as Binary into variable r 
+    # Read as Binary into variable r
     with open(input_path, 'rb') as input_file:
         input_binary = input_file.read()
 
