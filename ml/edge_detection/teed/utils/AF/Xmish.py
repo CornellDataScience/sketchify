51c4c7c29dc6f520a5962d0c88d14cae6361071f
Darren Key <dyk34@cornell.edu>
2024-04-15 16:52:01 -0400

    Edge detection (#23)
    * Hyperparams changed
    
    * Teed added
    
    * Move model to new folder and reformat
    
    * Delete extra model.py
    
    * Implement model.py for TEED
    
    * fix
    
    * Fix file import issues
    
    * Finally fix path issues
    
    * Fix main
    
    ---------
    
    Co-authored-by: Jason Zheng <jasonz4200@gmail.com>

---
+++ ml/edge_detection/teed/utils/AF/Xmish.py
@@ -0,0 +1,45 @@
+"""
+Applies the mish function element-wise:
+mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+"""
+
+# import pytorch
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+# import activation functions
+# import .utils.AF.Fmish as Func
+# from .utils.AF.Fmish import mish
+from .Fmish import mish as Funcmish
+
+
+class Mish(nn.Module):
+    """
+    Applies the mish function element-wise:
+    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+    Shape:
+        - Input: (N, *) where * means, any number of additional
+          dimensions
+        - Output: (N, *), same shape as the input
+    Examples:
+        >>> m = Mish()
+        >>> input = torch.randn(2)
+        >>> output = m(input)
+    Reference: https://pytorch.org/docs/stable/generated/torch.nn.Mish.html
+    """
+
+    def __init__(self):
+        """
+        Init method.
+        """
+        super().__init__()
+
+    def forward(self, input):
+        """
+        Forward pass of the function.
+        """
+        if torch.__version__ >= "1.9":
+            return F.mish(input)
+        else:
+            return Funcmish(input)
