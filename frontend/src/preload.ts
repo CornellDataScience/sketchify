2d5b9c82f9a2659ce6740e579b0b862f61a14338
Jason Zheng <48730262+jasozh@users.noreply.github.com>
2024-04-21 19:51:52 -0400

    Create an Electron project with React that runs Python (#31)
    * New forge app
    
    * Remove yarn
    
    * Finish react integration
    
    * Format
    
    * Add first component
    
    * cleanup
    
    * Add Python support
    
    * Add Linux build target
    
    * Fix broken electron build
    
    * Fix Python not running in production
    
    * Delete old frontend

--- frontend/src/preload.ts
+++ frontend/src/preload.ts
@@ -1,3 +1,5 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
@@ -5,7 +7,7 @@ contextBridge.exposeInMainWorld("electronAPI", {
   runPythonScript: () => ipcRenderer.send("run-python-script"),
 
   // Function to subscribe to the Python script response IPC message
-  handlePythonScriptResponse: (callback) => {
+  handlePythonScriptResponse: (callback: any) => {
     ipcRenderer.on("python-script-response", (event, ...args) =>
       callback(...args)
     );
