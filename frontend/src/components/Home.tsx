c994399e7aeb54d4d4380fb413ebba07f4c8e203
Jason Zheng <jasonz4200@gmail.com>
2024-04-21 16:46:47 -0400

    Add Python support

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react";
+
 const Home = () => {
+  const [output, setOutput] = useState("");
+
+  const handleClick = () => {
+    window.electronAPI.runPythonScript();
+
+    window.electronAPI.handlePythonScriptResponse((message: any) => {
+      setOutput(message);
+    });
+  };
+
   return (
     <>
       <div>Hello cool!</div>
+      {output}
+      <button onClick={handleClick}>asdf</button>
     </>
   );
 };
