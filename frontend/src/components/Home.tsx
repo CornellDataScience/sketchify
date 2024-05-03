e877f308872e72fa1bde42234f405baa9baff07c
Jason Zheng <jasonz4200@gmail.com>
2024-05-02 21:47:12 -0400

    Add frontend code

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,7 +1,16 @@
 import { useState, useEffect } from "react";
 
-const Home = () => {
+const Home: React.FC = () => {
   const [output, setOutput] = useState("");
+  const [ogImage, setOgImage] = useState<{ bytes: string; path: string }>({
+    bytes: "",
+    path: "",
+  });
+  const [sketchImage, setSketchImage] = useState<{
+    bytes: string;
+    path: string;
+  }>({ bytes: "", path: "" });
+  const [similarityScore, setSimilarityScore] = useState("");
 
   const handleClick = () => {
     window.electronAPI.runPythonScript();
@@ -11,9 +20,56 @@ const Home = () => {
     });
   };
 
+  const uploadImage = () => {
+    window.electronAPI.runUploadImage();
+
+    window.electronAPI.handleImageSetResponse((message: any) => {
+      setOgImage(message);
+    });
+  };
+
+  const uploadSketch = () => {
+    window.electronAPI.runUploadSketch();
+
+    window.electronAPI.handleSketchSetResponse((message: any) => {
+      setSketchImage(message);
+    });
+  };
+
+  const checkSimilarity = () => {
+    if (ogImage && sketchImage) {
+      window.electronAPI.runImageSimilarity(ogImage, sketchImage);
+    }
+
+    window.electronAPI.handleSimilarityResponse((message: any) => {
+      setSimilarityScore(message);
+    });
+  };
+
   return (
     <>
-      <div>Hello cool!</div>
+      <button onClick={uploadImage} id="upload-image">
+        Upload Image
+      </button>
+      <button onClick={uploadSketch} id="upload-sketch">
+        Upload Sketch
+      </button>
+      <button
+        onClick={checkSimilarity}
+        disabled={ogImage.path == "" || sketchImage.path == ""}
+        id="check-similarity"
+        className="checkSimilarity"
+      >
+        Check Similarity
+      </button>
+
+      <h4 id="similarity-score">Similarity Score: {similarityScore}</h4>
+
+      <img src={`data:image/jpg;base64,${ogImage.bytes}`} alt={ogImage.path} />
+      <img
+        src={`data:image/jpg;base64,${sketchImage.bytes}`}
+        alt={sketchImage.path}
+      />
       {output}
       <button onClick={handleClick}>asdf</button>
     </>
