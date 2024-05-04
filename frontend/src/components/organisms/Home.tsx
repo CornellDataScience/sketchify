23da14f0d37a50b10e65335513d9daa9c3a754c0
cjt76 <cjt76@cornell.edu>
2024-05-04 12:32:45 -0400

    add welcome page

--- frontend/src/components/organisms/Home.tsx
+++ frontend/src/components/organisms/Home.tsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import Model from "./Model";
 import ImageSimilarity from "./ImageSimilarity";
+import Welcome from "./Welcome";
 import Button from "../atoms/Button";
 
-type NavItem = "MODEL" | "IMAGE SIMILARITY";
+type NavItem = "WELCOME" | "MODEL" | "IMAGE SIMILARITY";
 
 const Home = () => {
-  const [nav, setNav] = useState<NavItem>("MODEL");
+  const [nav, setNav] = useState<NavItem>("WELCOME");
   return (
     <div>
       {/* Nav bar */}
       CURRENT TAB: {nav}
       <br />
+      <Button disabled={nav === "WELCOME"} onClick={() => setNav("WELCOME")}>
+        Welcome
+      </Button>
       <Button disabled={nav === "MODEL"} onClick={() => setNav("MODEL")}>
         Model
       </Button>
@@ -22,6 +26,7 @@ const Home = () => {
         Image Similarity
       </Button>
       <hr />
+      {nav === "WELCOME" && <Welcome />}
       {nav === "MODEL" && <Model />}
       {nav === "IMAGE SIMILARITY" && <ImageSimilarity />}
     </div>
