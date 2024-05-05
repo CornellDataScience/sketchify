2f8872ce0a8ead42cb1d6b38be6874d8a377ad45
cjt76 <cjt76@cornell.edu>
2024-05-05 12:16:34 -0400

    add welcome page to current design

--- frontend/src/components/organisms/Welcome.tsx
+++ frontend/src/components/organisms/Welcome.tsx
@@ -6,7 +6,7 @@ const Welcome = () => {
           Not Sketchy
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-          Transform your photos into custom coloring pages
+          Transform your photos into custom coloring book pages!
         </p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
           {/* <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
